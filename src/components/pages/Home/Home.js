30c325a9218440381c54ab111b0adebc3e6f433a
Joey Petrone <joey.petrone@gmail.com>
2020-06-02 18:03:13 -0500

    routing complete

--- src/components/pages/Home/Home.js
+++ src/components/pages/Home/Home.js
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import { Link } from 'react-router-dom';
+
 import './Home.scss';
 
 class Home extends React.Component {
@@ -14,6 +16,8 @@ class Home extends React.Component {
       <div className="Home">
         <h1>Home</h1>
         <button className="btn btn-dark" onClick={this.editEvent}>Edit a thing</button>
+        <Link to='/scatts/scatt12345'>View Single</Link>
+        <Link to='/scatts/new'>New Scatt</Link>
       </div>
     );
   }
