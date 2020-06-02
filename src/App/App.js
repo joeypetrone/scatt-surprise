f7831b6f1ba6f36426835342228f0cb9bb68afb7
Joey Petrone <joey.petrone@gmail.com>
2020-06-01 19:52:12 -0500

    auth works

--- src/App/App.js
+++ src/App/App.js
@@ -1,12 +1,40 @@
 import React from 'react';
+import firebase from 'firebase/app';
+import 'firebase/auth';
 import './App.scss';
 
+import Auth from '../components/pages/Auth/Auth';
+import MyNavbar from '../components/shared/MyNavbar/MyNavbar';
+
+import fdConnection from '../helpers/data/connection';
+
+fdConnection();
+
 class App extends React.Component {
+  state = {
+    authed: false,
+  }
+
+  componentDidMount = () => {
+    this.removeListener = firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        this.setState({ authed: true });
+      } else {
+        this.setState({ authed: false });
+      }
+    });
+  }
+
+  componentWillUnmount() {
+    this.removeListener();
+  }
+
   render() {
     return (
       <div className="App">
-        <h2 className="mt-3">Scatt Surprise React App</h2>
-        <button className="btn btn-info">Info Button</button>
+        <MyNavbar />
+        <h1 className="mt-3">Scatt Surprise React App</h1>
+        <Auth />
       </div>
     );
   }
