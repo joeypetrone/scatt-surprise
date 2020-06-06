9cf796b88e64b1c7cee83897030628acb7298ad0
Joey Petrone <joey.petrone@gmail.com>
2020-06-06 11:25:06 -0500

    added buttons to scatt card

--- src/components/shared/ScattCard/ScattCard.js
+++ src/components/shared/ScattCard/ScattCard.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 import scattShape from '../../../helpers/propz/scattShape';
 
@@ -12,11 +13,16 @@ class ScattCard extends React.Component {
 
   render() {
     const { scatt } = this.props;
+    const singleLink = `/scatts/${scatt.id}`;
+    const editLink = `/edit/${scatt.id}`;
+
     return (
       <div className="ScattCard col-3">
         <div className="card">
           <div className="card-body">
           <h5 className="card-title">{scatt.location}</h5>
+          <Link className="btn btn-danger" to={singleLink}>View</Link>
+          <Link className="btn btn-danger" to={editLink}>Edit</Link>
           <p className="card-text">{scatt.notes}</p>
         </div>
       </div>
